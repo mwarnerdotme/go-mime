f6712076f7ded5541f3fc3b4fe28ab41b39a8fd8
Michael Warner <michael@mwarner.me>
2021-05-30 10:40:01 -0600

    added root type and mime type agg slices

--- mime/mime_type.go
+++ mime/mime_type.go
@@ -13,6 +13,17 @@ const (
 	ROOT_TYPE_VIDEO       RootType = "video"
 )
 
+// Slice containing all available root MIME types
+var RootTypes = []RootType{
+	ROOT_TYPE_APPLICATION,
+	ROOT_TYPE_AUDIO,
+	ROOT_TYPE_FONT,
+	ROOT_TYPE_IMAGE,
+	ROOT_TYPE_MULTIPART,
+	ROOT_TYPE_TEXT,
+	ROOT_TYPE_VIDEO,
+}
+
 // Returns the root type as a string.
 func (rt *RootType) String() string {
 	return string(*rt)
@@ -20,6 +31,21 @@ func (rt *RootType) String() string {
 
 // A MIME type (content type in HTTP).
 type MimeType string
+
+// Slice containing all available MIME types
+var MimeTypes = func() []MimeType {
+	var mt []MimeType
+
+	mt = append(mt, ApplicationMIMETypes...)
+	mt = append(mt, AudioMIMETypes...)
+	mt = append(mt, FontMIMETypes...)
+	mt = append(mt, ImageMIMETypes...)
+	mt = append(mt, MultipartMIMETypes...)
+	mt = append(mt, TextMIMETypes...)
+	mt = append(mt, VideoMIMETypes...)
+
+	return mt
+}()
 
 // Returns the mime type as a string.
 func (mt *MimeType) String() string {
